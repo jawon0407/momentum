d949468708047f4ca6d56622ec21136cfed703f9
jawon0407 <jawon0407@naver.com>
2022-01-24 22:55:04 +0900

    update momentum-clonecoding

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -3,25 +3,32 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
 let toDos = [];
-//array 생성
+//array 생성 WHY let? todo의 목록은 항상 바뀔수 있기 때문!
 
 const TODOS_KEY = "todo";
+
+toDoForm.addEventListener("submit",handleToDoSubmit);
 
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
-    toDos.push(newTodo);
+    const newToDoObj = {
+        text:newTodo,
+        id:Date.now(),
+    };
+    toDos.push(newToDoObj);
     //newTodo 변수값을 toDos = []; array의 값들로 지정
-    paintToDo(newTodo);
+    paintToDo(newToDoObj);
     saveToDos();
 }
-toDoForm.addEventListener("submit",handleToDoSubmit);
+
 
 function paintToDo(todoIt){
     const li = document.createElement("li");
+    li.id=todoIt.id;
     const span = document.createElement("span");
-    span.innerText = todoIt;
+    span.innerText = todoIt.text;
     const button = document.createElement("button");
     button.innerText="❌"
     button.addEventListener("click",deleteToDo);
@@ -34,6 +41,9 @@ function paintToDo(todoIt){
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
+    console.log(li.id);
+    toDos = toDos.filter((toDo)=> toDo.id !== parseInt(li.id))
+    saveToDos();
 }
 
 function saveToDos(){
@@ -53,3 +63,4 @@ if(savedToDos !== null ){
 }
 
 //stringify으로 한번 거치고 parse를 통해 array값으로 저장하는 이유 => localStorage에 저장 될 때 바로 array로 저장 할 수 없어서 한번 stringify으로 한번 거쳐줘야한다.
+
