82781b7b2574f7cc1d482f8527aae9dcc6d715f4
jawon0407 <jawon0407@naver.com>
2022-02-19 03:08:14 +0900

    momentum

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,17 @@
+const API_KEY="7733420f0fe701f04a05f427d35ab20d";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(`You live in ${lat}, ${lng}`);
-    api.openweathermap.org/data/2.5/weather?lat=38.1469&lon=128.2472&appid=7733420f0fe701f04a05f427d35ab20d
+    const lon = position.coords.longitude;
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main},${data.main.temp}`;
+    });
 }
 
 function onGeoError(){
